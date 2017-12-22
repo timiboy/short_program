d52a9e82cce896a488908e0388e9742f0a5397e7
fish <1464250028@qq.com>
2017-12-22 11:38:50 +0800

    添加递归转化为html table的函数

--- analysis_json.py
+++ analysis_json.py
@@ -4,6 +4,10 @@ import json
 import traceback
 from datetime import datetime, timedelta
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 ##########################################
 # 递归解析json的工具类
 ##########################################
@@ -11,26 +15,72 @@ class RecurseTranslater():
     def __init__(self, data):
         self.stack_dict = {}
         self.data = data
+        self.table_str = ''
+
+    # 转化为表格
+    def convert_to_table(self):
+        html_start = '<table class="table"><tbody>'
+        html_end = '</tbody></table>'
+        self.recuse_convert(self.data)
+        self.table_str = html_start + self.table_str + html_end
+
+    # 递归转化
+    def recuse_convert(self, obj):
+        blank_line = '<tr><td>&nbsp;</td><td>&nbsp;</td></tr>'
+        if type(obj) == dict:
+            ordered = sorted([(k, v) for k, v in obj.items()], key=lambda x: x[1], reverse=True)
+            for k, v in ordered:
+                if type(v) != dict and type(v) != list:
+                    self.table_str = self.table_str + '<tr><td>' + str(k) + '  :  ' + '</td><td>' + str(v) + '</td></tr>'
+                else:
+                    if not self.table_str.endswith(blank_line):
+                        self.table_str = self.table_str + blank_line
+                    self.recuse_convert(v)
+        if type(obj) == list:
+            for count, each in enumerate(obj):
+                if count != 0:
+                    self.table_str = self.table_str + blank_line
+                self.recuse_convert(each)
+                if count == len(obj) - 1:
+                    self.table_str = self.table_str + blank_line
+
+    # 解释值
+    def judge_key(self, k, obj, key):
+        if k == 'hasSystemError':
+            obj[key] = {'true': u'有错误', 'false': u'无错误'}.get(obj[key], obj[key])
+        if k == 'isFrozen':
+            obj[key] = {'true': u'被冻结', 'false': u'未被冻结'}.get(obj[key], obj[key])
+        if k == 'message':
+            obj[key] = {'SUCCESS': u'成功'}.get(obj[key], obj[key])
+        if k == 'class':
+            obj.pop(k)
+        if obj.get(key, '-1') == '' or obj.get(key, '-1') == None:
+            obj[key] = u'\\'
 
     # 解析
     def analysis(self):
         self.recuse_analysis(self.data, self.stack_dict)
+
     # 翻译
     def translate(self):
         self.recuse_translate(self.data, MAP)
 
+    # 递归翻译
     def recuse_translate(self, obj, map):
         if type(obj) == dict:
             for k, v in obj.items():
                 if type(v) != dict and type(v) != list:
-                    if map.get(k, ''):
-                        obj[map[k]] = obj.pop(k)
+                    key = map.get(k, '')
+                    if key:
+                        obj[key] = obj.pop(k)
+                    self.judge_key(k, obj, key)
                 else:
                     self.recuse_translate(v, map[k])
         if type(obj) == list:
             for each in obj:
                 self.recuse_translate(each, map)
 
+    # 递归解析
     def recuse_analysis(self, obj, stack):
         if type(obj) == dict:
             for k, v in obj.items():
@@ -144,3 +194,5 @@ if __name__ == '__main__':
     rt.analysis()
     rt.translate()
     print rt.data
+    rt.convert_to_table()
+    print rt.table_str
